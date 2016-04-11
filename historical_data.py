57296dbf27f09282248f04d0d3b6453d13a68816
trevor smith <trevor.stuard@gmail.com>
2016-04-10 17:28:27 -0700

    added game log functionality

--- historical_data.py
+++ historical_data.py
@@ -2,9 +2,6 @@ import requests
 import pandas as pd
 import numpy as np
 
-player_ids_url = 'http://stats.nba.com/stats/commonallplayers' + \
-            '?IsOnlyCurrentSeason=1&LeagueID=00&Season=2015-16'
-
 class HistoricalData():
     """Retrieves historical data from NBA.com for a specified player and
     season.  By default any function in this class will grab all player
@@ -20,38 +17,69 @@ class HistoricalData():
                 season.  Ex: '2014-15' is the 2014/2015 season.
     """
 
-    def __init__(self, players, season='2015-16'):
-        self.players = players
+    global AGENT_DETAILS
+
+    AGENT_DETAILS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64;\
+    rv:430) Gecko/20100101'}
+
+    def __init__(self, season='2015-16'):
         self.season = season
 
-    def game_logs()
+    def get_player_ids(season='2015-16', headers=AGENT_DETAILS):
+        """This function returns a dictionary of player names and
+        player ids.  The default season is 2015/2016.
 
-# request the URL and parse the JSON
-response = requests.get(player_ids_url)
-response.raise_for_status() # raise exception if invalid response
-player_ids = response.json()['resultSets'][0]['rowSet']
+        Ex: '{'Quincy Acy': 203112,
+              'Steven Adams': 203500}
 
-player_lookup = []
-for player in player_ids:
-    player_lookup.append([player[7], player[2]])
+        Change Season:
+                season: specify 'yyyy-yy' format.  The four digit year is the
+                start of the season.  The two digit year is the end of the
+                season.  Ex: '2014-15' is the 2014/2015 season.
+        """
 
-def game_logs(player_id, season='2015-16'):
-    url = 'http://stats.nba.com/stats/playergamelog?LeagueID=00&' + \
-            'PlayerID=%s&Season=%s&SeasonType=Regular+Season' \
-            % (player_id, season)
-    return url
+        print season
 
-headers = response.json()['resultSets'][0]['headers']
+        player_ids_url = 'http://stats.nba.com/stats/commonallplayers' + \
+            '?IsOnlyCurrentSeason=1&LeagueID=00&Season=%s' % (season)
 
-data = []
-bad_ids = []
-for i in player_lookup[:2]:
-    pid = i[0]
-    try:
-        response = requests.get(game_logs(pid))
-        results = response.json()['resultSets'][0]['rowSet']
-        for row in results:
-            data.append(row)
-    except:
-        print 'bad request: ' + str(pid)
-        bad_ids.append(pid)
+        print player_ids_url
+        response = requests.get(player_ids_url, headers=AGENT_DETAILS)
+        response.raise_for_status()
+
+        print 'downloading player ids for season: ' + season
+
+        players = response.json()['resultSets'][0]['rowSet']
+        player_ids = {}
+
+        for player in players:
+            player_ids[player[2]] = player[0]
+
+        return player_ids
+
+    def game_logs(season='2015-16', players=get_player_ids(), headers=AGENT_DETAILS):
+            """Retrieves the game logs for the player list specified.  This will
+            return a list of lists of player data.  The default season is
+            2015-16 and it retrieves for players active during that season.
+            """
+
+            data = []
+
+            for k, v in players.iteritems():
+                game_logs_url = 'http://stats.nba.com/stats/playergamelog?' + \
+                'LeagueID=00&PlayerID=%s&Season=%s&SeasonType=Regular+Season' \
+                % (v, season)
+                print 'extracting data for: ' + str(k) + ' ' + str(v)
+                print game_logs_url
+                # response = requests.get(game_logs_url, headers=AGENT_DETAILS)
+                # results = response.json()['resultSets'][0]['rowSet']
+                # try:
+                    # for game in results:
+                        # data.append(game)
+                # except:
+                    # print 'bad request'
+
+            # return data
+
+if __name__ == "__main__":
+    main()
