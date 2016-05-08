d523d695cfb6208e3fab4a9d0113574bb0905744
trevor smith <trevor.stuard@gmail.com>
2016-05-07 23:13:06 -0700

    updating self usage

--- historical_data.py
+++ historical_data.py
@@ -19,13 +19,17 @@ class HistoricalData():
 
     global AGENT_DETAILS
 
-    AGENT_DETAILS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64;\
-    rv:430) Gecko/20100101'}
+    AGENT_DETAILS = {'user-agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/45.0.2454.101 Safari/537.36'),
+                     'referer': 'http://stats.nba.com/scores/'
+                    }
 
-    def __init__(self, season='2015-16'):
+    def __init__(self, season='2015-16', headers=AGENT_DETAILS):
         self.season = season
+        self.headers = headers
 
-    def get_player_ids(season='2015-16', headers=AGENT_DETAILS):
+    def get_player_ids(self):
         """This function returns a dictionary of player names and
         player ids.  The default season is 2015/2016.
 
@@ -39,12 +43,10 @@ class HistoricalData():
         """
 
         player_ids_url = 'http://stats.nba.com/stats/commonallplayers' + \
-            '?IsOnlyCurrentSeason=1&LeagueID=00&Season=%s' % (season)
+            '?IsOnlyCurrentSeason=1&LeagueID=00&Season=%s' % (self.season)
 
-        response = requests.get(player_ids_url, headers=AGENT_DETAILS)
+        response = requests.get(player_ids_url, headers = self.headers)
         response.raise_for_status()
-
-        print 'downloading player ids for season: ' + season
 
         players = response.json()['resultSets'][0]['rowSet']
         player_ids = {}
@@ -54,21 +56,21 @@ class HistoricalData():
 
         return player_ids
 
-    def game_logs(season='2015-16', players=get_player_ids(), headers=AGENT_DETAILS):
+    def game_logs(self):
             """Retrieves the game logs for the player list specified.  This will
             return a list of lists of player data.  The default season is
             2015-16 and it retrieves for players active during that season.
             """
 
+            players = self.get_player_ids()
             data = []
 
             for k, v in players.iteritems():
                 game_logs_url = 'http://stats.nba.com/stats/playergamelog?' + \
                 'LeagueID=00&PlayerID=%s&Season=%s&SeasonType=Regular+Season' \
-                % (v, season)
-                print 'extracting data for: ' + str(k) + ' ' + str(v)
-                print game_logs_url
-                response = requests.get(game_logs_url, headers=AGENT_DETAILS)
+                % (v, self.season)
+
+                response = requests.get(game_logs_url, headers = self.headers)
                 results = response.json()['resultSets'][0]['rowSet']
                 try:
                     for game in results:
@@ -78,5 +80,8 @@ class HistoricalData():
 
             return data
 
-if __name__ == "__main__":
+def main():
+    pass
+
+if __name__ == '__main__':
     main()
